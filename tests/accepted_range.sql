6fe2cf1f0877ad57c29fd378470ebeb0df20ceb7
Anna Yastremska <ayastremska@kse.org.ua>
2026-04-10 20:12:28 +0000

    accepted range test

---
+++ tests/accepted_range.sql
@@ -0,0 +1,16 @@
+{% test accepted_range(model, column_name, min_value=None, max_value=None) %}
+
+select {{ column_name }}
+from {{ model }}
+where
+    {% if min_value is not none and max_value is not none %}
+        {{ column_name }} < {{ min_value }} or {{ column_name }} > {{ max_value }}
+    {% elif min_value is not none %}
+        {{ column_name }} < {{ min_value }}
+    {% elif max_value is not none %}
+        {{ column_name }} > {{ max_value }}
+    {% else %}
+        false  -- no bounds specified, test always passes
+    {% endif %}
+
+{% endtest %}
